08c24186e97efdb0cf5f042742fd15f651d6fee8
Anubhav Maity <anubhavmaity7@gmail.com>
2025-03-19 03:35:49 +0000

    deploy to elastic beanstalk

--- 04-deployment/web-service/test.py
+++ 04-deployment/web-service/test.py
@@ -3,12 +3,15 @@ import json
 
 ride = {
     "PULocationID": 10,
-    "DOLocationID": 50, 
-    "trip_distance": 40
+    "DOLocationID": 10, 
+    "trip_distance": 0
 }
 
-url = 'http://127.0.0.1:9696/predict'
-url = 'http://0.0.0.0:9696/predict'
+HOST = '127.0.0.1:9696'
+HOST = '0.0.0.0:9696' 
+HOST = 'ride-duration-prediction-service.eba-wqx6eh2n.us-west-2.elasticbeanstalk.com' # Elastic Beanstalk Host
+url = f'http://{HOST}/predict'
+
 response = requests.post(url, json = json.dumps(ride))
 print(response.json())
 
